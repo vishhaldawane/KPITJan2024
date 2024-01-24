743ab634c43641711e86795d30e7c27b26521046
Prakhar Shukla <100852819+Prakharshuklaaa@users.noreply.github.com>
2024-01-24 11:24:02 +0530

    SpringSession1
    Jan24

---
+++ SpringDemo/src/main/java/com/kpit/HotelRoom.java
@@ -0,0 +1,9 @@
+package com.kpit;
+public class HotelRoom {
+	public HotelRoom() {
+		System.out.println("HotelRoom() ctor..."+this.hashCode());
+	}
+	void enjoyTelevision() {
+		System.out.println("Enjoying tv...");
+	}
+}
---
+++ SpringDemo/src/main/java/com/kpit/SpringTest.java
@@ -0,0 +1,34 @@
+package com.kpit;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+	public static void main(String[] args) {
+		System.out.println("Begin main");
+		System.out.println("==>Creating application context...");
+			ApplicationContext ctx = new ClassPathXmlApplicationContext("myspring.xml");
+		System.out.println("=>ApplicationContext created....");
+
+		
+		HotelReception reception = (HotelReception) ctx.getBean("hotelRec");
+		reception.checkRooms();
+		reception.billing();
+		
+		HotelRoom room1 = (HotelRoom) ctx.getBean("hotelRm");
+		HotelRoom room2 = (HotelRoom) ctx.getBean("hotelRm");
+		HotelRoom room3 = (HotelRoom) ctx.getBean("hotelRm");
+		
+		room1.enjoyTelevision();
+		room2.enjoyTelevision();
+		room3.enjoyTelevision();
+		
+		System.out.println("reception : "+reception.hashCode());
+		System.out.println("room1 : "+room1.hashCode());
+		System.out.println("room1 : "+room2.hashCode());
+		System.out.println("room1 : "+room3.hashCode());
+		
+		
+		System.out.println("End main");
+	}
+}
